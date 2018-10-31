826028fecc0842404daf1fe5b0eb3cb24406a801
MattWarren97 <mfw1g15@soton.ac.uk>
2018-10-31 01:16:16 +0000

    tried to get it to take entire instruction durations (not every single physics step) in the Learner. I've done something wrong and its going horribly wrong at doing that

--- GroundLearner.py
+++ GroundLearner.py
@@ -7,29 +7,59 @@ carLocX_1, carLocY_1, carDirX_1, carDirY_1, carVelX_1, carVelY_1,
 throttle, steer, time
  """
 
+def identicalLists(A, B):
+	for i, a in enumerate(A):
+		if a != B[i]:
+			return False
+	return True
+
+
 def readCSV():
 	fileName = "MovementData/1540944895.429397.csv"
 	features = []
 	targets = []
+	prevInstr = []
+	instrTime = 0
+	instrTargets = []
+	instrFeatures = []
 	with open(fileName, 'r') as csvFile:
 		dataReader = csv.reader(csvFile, lineterminator='j')
 		dataFormat = next(dataReader)
 
 		for c, row in enumerate(dataReader):
-			f = []
-			t = []
-			f.extend(row[0:15])
-			t.extend(row[15:30])
-			f.extend(row[30:33])
-			#print("Fetaures: ", f)
-			#print("Targets: ", t)
+			row = [float(i) for i in row]
+			if not prevInstr:
+				prevInstr = row[30:32] #elements 30 and 31 are throttle and steer
+			else:
+				instructions = row[30:32]
+				if identicalLists(instructions, prevInstr):
+					instrTime += row[32]
+					instrTargets = row[21:30] 
+				else:
+					#if we have reached teh end of the instruction
+					if instrFeatures:
+						#first instruction is ignored, otherwise add features, targets to lists.
+						instrFeatures.append(instrTime)
+						features.append(instrFeatures)
+						targets.append(instrTargets)
+						instrFeatures = row[6:15]
+						instrFeatures.extend(instructions)
+						prevInstr = instructions
+					prevInstr = instructions
+					instrFeatures = row[6:15]
+					instrFeatures.extend(instructions)
+					instrTime = 0
+
+
 			if c%1000 ==0:
 				print("C: ", c)
-				print("Features: ", f)
-				print("Targets: ", t, "\n\n")
-				#print("Row: ", row, "\n\n\n")
+
 
 		print(dataFormat)
+		for i, f in enumerate(features):
+			print("Feature ", i, ": ", f, "\nMapsTo: Target: ", targets[i], "\n\n")
+		print("Length  of feautres, targets is: ", len(features), " - ", len(targets))
+
 
 			
 			
