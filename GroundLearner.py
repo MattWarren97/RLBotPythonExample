3b550233374a0aa5c222b50d91bfc449c09bbd2f
MattWarren97 <mfw1g15@soton.ac.uk>
2018-11-23 02:11:00 +0000

    modified GroundLearner to work with the gameSpeed_in_title files... It now has lots more data! These results are still an absolute abomination

--- GroundLearner.py
+++ GroundLearner.py
@@ -27,16 +27,20 @@ class GroundLearner:
 		#ignore the ball; carPrevState is 6:15, carAfterState is 21:30
 		f = row[6:15]
 		f.extend(row[21:30])
+		f.append(self.getTime(row))
+
 		return f
 
 	def getHitBallFeatures(self, row):
 		f = row[6:15]
 		f.extend(row[21:24]) #ignores final car orientation and velocity.
+		f.append(self.getTime(row))
 		return f
 	
 	def getLearnCarPosFeatures(self, row):
 		f = row[6:15]
-		f.extend(row[30:33])
+		f.extend(row[30:32])
+		f.append(self.getTime(row))
 		return f
 
 	def getLearnCarPosTargets(self, row):
@@ -44,13 +48,17 @@ class GroundLearner:
 
 	def getTargets(self, row):
 		#instructions are 30:32, time is 32.
-		return row[30:33]
+		t = row[30:32]
+		#t.append(self.getTime(row))
+		return t
 
 	def getInstructions(self, row):
 		return row[30:32] #30: throttle, 31: steer
 
 	def getTime(self, row):
-		return row[32]
+		return row[32]*self.gameSpeed
+
+
 
 	def readCSV(self):
 		self.features = []
@@ -63,7 +71,17 @@ class GroundLearner:
 		for f in os.listdir(self.dirName):
 			fileName = self.dirName + f
 			with open(fileName, 'r') as csvFile:
+				self.gameSpeed = 1
 				print("Reading inputs from ", fileName)
+				speedLoc = fileName.find("Speed")
+				if speedLoc == -1:
+					self.gameSpeed = 1
+				else:
+					#"Speed" is found in the file name -- so speedLoc+5 is the gameSpeed
+					#just one character, because it will always be 5 or 1 for now
+					self.gameSpeed = float(fileName[speedLoc+5])
+
+
 				dataReader = csv.reader(csvFile, lineterminator='')
 				dataFormat = next(dataReader)
 
@@ -110,6 +128,8 @@ class GroundLearner:
 		dataScaler = StandardScaler()
 		dataScaler.fit(f_train)
 
+		self.mlpScaler = dataScaler
+
 		f_train = dataScaler.transform(f_train)
 		f_test = dataScaler.transform(f_test)
 
@@ -120,6 +140,8 @@ class GroundLearner:
 
 		print("Training score: ", self.mlp.score(f_train, t_train))
 		print("Testing score: ", self.mlp.score(f_test, t_test), "\n")
+		self.testFeatures = f_test
+		self.testTargets = t_test
 		return self.mlp
 
 	def trainLinearRegressor(self):
@@ -134,6 +156,7 @@ class GroundLearner:
 		print("Size of testing set is: f, t: ", len(f_test), ", ", len(t_test))
 		dataScaler = StandardScaler()
 		dataScaler.fit(f_train)
+
 
 		f_train = dataScaler.transform(f_train)
 		f_test = dataScaler.transform(f_test)
@@ -168,8 +191,8 @@ class GroundLearner:
 		print("Training score: ", self.hbMLP.score(f_train, t_train))
 		print("Testing score: ", self.hbMLP.score(f_test, t_test), "\n")
 
-		self.testFeatures = f_test
-		self.testTargets = t_test
+		self.hbTestFeatures = f_test
+		self.hbTestTargets = t_test
 
 		return self.hbMLP
 
@@ -196,18 +219,18 @@ class GroundLearner:
 		print("Training score: ", self.lcpMLP.score(f_train, t_train))
 		print("Testing score: ", self.lcpMLP.score(f_test, t_test), "\n")
 
-		print("\n--------With SCALED target data: --------\n")
-		targetScaler = StandardScaler()
-		targetScaler.fit(t_train)
-		t_train = targetScaler.transform(t_train)
-		t_test = targetScaler.transform(t_test)
+		#print("\n--------With SCALED target data: --------\n")
+		#targetScaler = StandardScaler()
+		#targetScaler.fit(t_train)
+		#t_train = targetScaler.transform(t_train)
+		#t_test = targetScaler.transform(t_test)
 
-		self.lcpMLP = MLPRegressor(early_stopping=True, hidden_layer_sizes=(64,16,8), max_iter=10000)
+		#self.lcpMLP = MLPRegressor(early_stopping=True, hidden_layer_sizes=(64,16,8), max_iter=10000)
 
-		self.lcpMLP.fit(f_train, t_train)
+		#self.lcpMLP.fit(f_train, t_train)
 
-		print("Training score: ", self.lcpMLP.score(f_train, t_train))
-		print("Testing score: ", self.lcpMLP.score(f_test, t_test), "\n")
+		#print("Training score: ", self.lcpMLP.score(f_train, t_train))
+		#print("Testing score: ", self.lcpMLP.score(f_test, t_test), "\n")
 
 		"""predictions = self.lcpMLP.predict(f_test[0:20])
 
@@ -216,6 +239,9 @@ class GroundLearner:
 			print("Target: ", t_test[i])
 			print("Made prediction: ", p)
 			print("\n")"""
+
+		self.lcpTestFeatures = f_test
+		self.lcpTestTargets = t_test
 
 
 		return self.lcpMLP
@@ -228,18 +254,37 @@ def main():
 
 	gl = GroundLearner(dirName)
 	#gl.trainHitBallMLP()
+	gl.trainMLPRegressor()
 	gl.trainLCP_MLP()
 	#gl.trainLinearRegressor()
-	#gl.trainMLPRegressor()
+	
 
-	#f1 = [gl.testFeatures[0]]
-	#print("First feature is: ", f1)
-	#t1 = gl.testTargets[0]
-	#print("First target is: ", t1)
 
-	#prediction = gl.hbMLP.predict(f1)
-	#print("Prediction is: ", prediction)
+	"""
+	f1 = [gl.hbTestFeatures[0]]
+	print("First feature is: ", f1)
+	t1 = gl.hbTestTargets[0]
+	print("First target is: ", t1)
+	prediction = gl.hbMLP.predict(f1)
+	print("Prediction is: ", prediction)
+	"""
+	print("Predicting 3 values for Regular Instruction_Predictor")
+	for i in range(3):
+		fi = [gl.testFeatures[i]]
+		print("Feature ", i, " is: ", gl.mlpScaler.inverse_transform(fi))
+		ti = gl.testTargets[i]
+		print("Target ", i, " is: ", ti)
+		predictioni = gl.mlp.predict(fi)
+		print("Prediction is: ", predictioni)
 
+	print("Predicting 3 values for LearnCarPosition MLP")
+	for i in range(3):
+		fi = [gl.lcpTestFeatures[i]]
+		print("Feature ", i, " is: ", gl.lcpScaler.inverse_transform(fi))
+		ti = gl.lcpTestTargets[i]
+		print("Target ", i, " is: ", ti)
+		predictioni = gl.lcpMLP.predict(fi)
+		print("Prediction is: ", predictioni)
 
 
 if __name__ == "__main__":
